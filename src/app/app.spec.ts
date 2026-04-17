ab412b0f495664f4c213005e8fb0f8df26be8cec
unsubstantiated-Script <jusfulton@gmail.com>
2026-04-17 18:38:08 -0500

    Fixing file structures

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, first-angular-app');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-drills-max');
   });
 });
--- src/app/app.ts
+++ src/app/app.ts
@@ -5,8 +5,8 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('first-angular-app');
+  protected readonly title = signal('angular-drills-max');
 }
